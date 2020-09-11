67ce1e4192e2742bebb462f858a56449b4ed2810
öykü parlak <56317041+oykuparlakk@users.noreply.github.com>
2020-09-12 00:40:13 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,56 +3,56 @@ import java.util.ArrayList;
 public class Main {
       public static void main(String[] args) {
     	  
-    	  /*3. Bir hastanede enfeksiyon bölümünde 4 adet temizlik personeli, 20 adet hemþire ve 10 adet doktor çalýþmaktýr.
-    	   * Tüm personeli Personel sýnýfýndan türeterek (miras alaraka) her tür peronel için ayrý bir sýnýf yazýnýz.  
+    	  /*3. Bir hastanede enfeksiyon bÃ¶lÃ¼mÃ¼nde 4 adet temizlik personeli, 20 adet hemÃ¾ire ve 10 adet doktor Ã§alÃ½Ã¾maktÃ½r.
+    	   * TÃ¼m personeli Personel sinifindan tÃ¼reterek (miras alaraka) her tÃ¼r peronel iÃ§in ayri bir sinif yaziniz.  
  
-             a) Bu personellerin adýný ve soyadýný ekrana yazan programý yazýnýz.
-             b) Bu personellerin adýný, soyadýný ve kadrosunu (temizlik, hemþire veya doktor) bir önceki fonksiyonu
-             override ederek (miras üzerine yazarak) uygulayýnýz. 
-             c) Bu personellerin adýný, soyadýný ve kadrosunu override iþlemi yapmadan Personel sýnýfý içerinde yazan programý yazýnýz.
-             d) Bu personelleri ad ve soyada göre sýralayan programý yazýnýz. Bu programda sýralama yapmak için aþaðýdaki static fonksiyonu yazýnýz. 
+             a) Bu personellerin adini ve soyadini ekrana yazan programi yaziniz.
+             b) Bu personellerin adini, soyadini ve kadrosunu (temizlik, hemÃ¾ire veya doktor) bir Ã¶nceki fonksiyonu
+             override ederek (miras Ã¼zerine yazarak) uygulayiniz. 
+             c) Bu personellerin adini, soyadini ve kadrosunu override islemi yapmadan Personel sinifi iÃ§erinde yazan programi yaziniz.
+             d) Bu personelleri ad ve soyada gÃ¶re siralayan programi yaziniz. Bu programda siralama yapmak iÃ§in asagidaki static fonksiyonu yaziniz. 
  
-             public static void sýrala(){ ArrayList<Personel> personeller = new ArrayList();
+             public static void sirala(){ ArrayList<Personel> personeller = new ArrayList();
              //...Personel ekle...// Collections.sort(personeller);  } 
     	   */
     	  
-   	      System.out.print("öykü parlak-18360859019");
+   	      System.out.print("Ã¶ykÃ¼ parlak-18360859019");
 
     	  ArrayList<Personel> liste=new ArrayList<Personel>();
-    	  Doktor dr1=new Doktor("mustafa","yýldýz","doktor");
-    	  Doktor dr2=new Doktor("ayþe","keskin","doktor");
-    	  Doktor dr3=new Doktor("ali","gördeal","doktor");
-    	  Doktor dr4=new Doktor("feyza","küskün","doktor");
+    	  Doktor dr1=new Doktor("mustafa","yildiz","doktor");
+    	  Doktor dr2=new Doktor("ayse","keskin","doktor");
+    	  Doktor dr3=new Doktor("ali","gÃ¶rdeal","doktor");
+    	  Doktor dr4=new Doktor("feyza","kÃ¼skÃ¼n","doktor");
     	  Doktor dr5=new Doktor("melisa","sahin","doktor");
     	  Doktor dr6=new Doktor("mehmet","emin","doktor");           
     	  Doktor dr7=new Doktor("fatma","celep","doktor");
-    	  Doktor dr8=new Doktor("gülseren","temiz","doktor");
-    	  Doktor dr9=new Doktor("þefik","altun","doktor");
-    	  Doktor dr10=new Doktor("selim","kýrgýz","doktor");
+    	  Doktor dr8=new Doktor("gÃ¼lseren","temiz","doktor");
+    	  Doktor dr9=new Doktor("sefik","altun","doktor");
+    	  Doktor dr10=new Doktor("selim","kÃ½rgÃ½z","doktor");
           Hemsire hemsire1 = new Hemsire("ali","aykut","hemsire");
     	  Hemsire hemsire2 = new Hemsire("fadik","kelkit","hemsire");
-    	  Hemsire hemsire3 = new Hemsire("hüsniye","yenitur","hemsire");
-    	  Hemsire hemsire4 = new Hemsire("kibar","karaçay","hemsire");
-    	  Hemsire hemsire5 = new Hemsire("feyyaz","çelik","hemsire");
-    	  Hemsire hemsire6 = new Hemsire("melis","çetin","hemsire");
-    	  Hemsire hemsire7 = new Hemsire("cengiz","karakuþ","hemsire");
-    	  Hemsire hemsire8 = new Hemsire("aslý","sýký","hemsire");
-    	  Hemsire hemsire9 = new Hemsire("nuriye","yýldýz","hemsire");
-    	  Hemsire hemsire10= new Hemsire("kerem","yýldýrým","hemsire");
-    	  Hemsire hemsire11= new Hemsire("fidan","sarýgül","hemsire");
-    	  Hemsire hemsire12= new Hemsire("nurgül","kýzýltaþ","hemsire");
-    	  Hemsire hemsire13= new Hemsire("arzu","yazý","hemsire");
-    	  Hemsire hemsire14= new Hemsire("beren","sarý","hemsire");
+    	  Hemsire hemsire3 = new Hemsire("hÃ¼sniye","yenitur","hemsire");
+    	  Hemsire hemsire4 = new Hemsire("kibar","karaÃ§ay","hemsire");
+    	  Hemsire hemsire5 = new Hemsire("feyyaz","Ã§elik","hemsire");
+    	  Hemsire hemsire6 = new Hemsire("melis","Ã§etin","hemsire");
+    	  Hemsire hemsire7 = new Hemsire("cengiz","karakuÃ¾","hemsire");
+    	  Hemsire hemsire8 = new Hemsire("asli","saka","hemsire");
+    	  Hemsire hemsire9 = new Hemsire("nuriye","yaldiz","hemsire");
+    	  Hemsire hemsire10= new Hemsire("kerem","yildirim","hemsire");
+    	  Hemsire hemsire11= new Hemsire("fidan","sarigÃ¼l","hemsire");
+    	  Hemsire hemsire12= new Hemsire("nurgÃ¼l","kiziltepe","hemsire");
+    	  Hemsire hemsire13= new Hemsire("arzu","yazi","hemsire");
+    	  Hemsire hemsire14= new Hemsire("beren","sari","hemsire");
     	  Hemsire hemsire15= new Hemsire("bahar","has","hemsire");
     	  Hemsire hemsire16= new Hemsire("berfin","karayaprak","hemsire");
-    	  Hemsire hemsire17= new Hemsire("belgin","eroðlu","hemsire");
-    	  Hemsire hemsire18= new Hemsire("okan","türk","hemsire");
-    	  Hemsire hemsire19= new Hemsire("durmuþ","akdemir","hemsire");
-    	  Hemsire hemsire20= new Hemsire("gül","parlak","hemsire");
+    	  Hemsire hemsire17= new Hemsire("belgin","eroglu","hemsire");
+    	  Hemsire hemsire18= new Hemsire("okan","tÃ¼rk","hemsire");
+    	  Hemsire hemsire19= new Hemsire("durmus","akdemir","hemsire");
+    	  Hemsire hemsire20= new Hemsire("gÃ¼l","parlak","hemsire");
     	  Temizlik_personeli temizlik1 = new Temizlik_personeli("ceren","kaya","temizlik personeli");
-    	  Temizlik_personeli temizlik2 = new Temizlik_personeli("cemre","durlanýk","temizlik personeli");
-    	  Temizlik_personeli temizlik3 = new Temizlik_personeli("deniz","öztop","temizlik personeli");
-    	  Temizlik_personeli temizlik4 = new Temizlik_personeli("deniz","ünal","temizlik personeli");
+    	  Temizlik_personeli temizlik2 = new Temizlik_personeli("cemre","durlanÃ½k","temizlik personeli");
+    	  Temizlik_personeli temizlik3 = new Temizlik_personeli("deniz","Ã¶ztop","temizlik personeli");
+    	  Temizlik_personeli temizlik4 = new Temizlik_personeli("deniz","Ã¼nal","temizlik personeli");
     	  
     	  
 		  liste.add(dr1);
@@ -89,7 +89,7 @@ public class Main {
 		  liste.add(temizlik2);
 		  liste.add(temizlik3);
 		  liste.add(temizlik4);
-    	  System.out.println("Personeller listeye eklenmiþtir.");
+    	  System.out.println("Personeller listeye eklenmistir.");
     	  
     	  
     	 
